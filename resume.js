b6f2d066f4e8c4d8330ee92e43c43de0ee59b9c3
Vachana Mandattil <65472668+Vachana1992@users.noreply.github.com>
2020-08-03 13:16:26 -0400

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,98 @@
+
+
+$(document).ready(function(){
+	$("#box1").click(function(){
+	$("#para1").fadeToggle("slow");
+  });
+	$("#box2").click(function(){	
+	$("#para2").fadeToggle("slow");
+    
+  });
+  
+  $("#hideIt1").click(function(){
+	$(".Tskill").hide();
+	});
+	
+	$("#showIt1").click(function(){
+	$(".Tskill").show();
+	});
+	
+	$("#hideIt2").click(function(){
+	$(".Sskill").hide();
+	});
+	
+	$("#showIt2").click(function(){
+	$(".Sskill").show();
+	});
+	
+	$("#box3").click(function(){	
+	$("#para3").fadeToggle("slow");
+    
+  });
+  $("#box4").click(function(){
+	$("#para4").fadeToggle("slow");
+  });
+  
+  $("#fadein3").click(function(){
+	$("#edu1").fadeIn(3000);
+	});
+	
+	$("#fadeout3").click(function(){
+	$("#edu1").fadeOut(3000);
+	});
+	
+	 $("#fadein4").click(function(){
+	$("#edu2").fadeIn(500);
+	});
+	
+	$("#fadeout4").click(function(){
+	$("#edu2").fadeOut(500);
+	});
+	
+	 $("#fadein5").click(function(){
+	$("#edu3").fadeIn(1000);
+	});
+	
+	$("#fadeout5").click(function(){
+	$("#edu3").fadeOut(1000);
+	});
+	
+	 $("#fadein6").click(function(){
+	$("#edu4").fadeIn(100);
+	});
+	
+	$("#fadeout6").click(function(){
+	$("#edu4").fadeOut(100);
+	});
+	
+	$("#slideup1").click(function(){
+	$("#exp1").slideUp();
+	});
+	
+	$("#slidedown1").click(function(){
+	$("#exp1").slideDown();
+	});
+	
+	$("#slideup2").click(function(){
+	$("#exp2").slideUp();
+	});
+	
+	$("#slidedown2").click(function(){
+	$("#exp2").slideDown();
+	});
+	$("#box5").click(function(){
+	$("#para5").fadeToggle("slow");
+  });
+  $("#box6").click(function(){
+	$("#para6").fadeToggle("slow");
+  });
+   $("#box7").click(function(){
+	$("#para7").fadeToggle("slow");
+  });
+	
+	 $('.slider').bxSlider();
+	
+	});
+	
+	
+	
